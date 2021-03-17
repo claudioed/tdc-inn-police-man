a88424cf958016447bcf13069e65cff868c0eeb5
claudioed <claudioed.oliveira@gmail.com>
2021-03-17 07:54:11 -0300

    fix prometheus version

--- src/main/java/tech/claudioed/police/man/verticles/StartServer.java
+++ src/main/java/tech/claudioed/police/man/verticles/StartServer.java
@@ -1,28 +1,18 @@
 package tech.claudioed.police.man.verticles;
 
-import grpc.health.v1.HealthGrpc;
-import grpc.health.v1.HealthOuterClass;
 import io.grpc.ServerInterceptors;
-import io.grpc.stub.StreamObserver;
 import io.jaegertracing.internal.JaegerTracer;
 import io.opentracing.Tracer;
 import io.opentracing.contrib.grpc.TracingServerInterceptor;
 import io.opentracing.util.GlobalTracer;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.grpc.VertxServerBuilder;
 import me.dinowernli.grpc.prometheus.Configuration;
 import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
-import tech.claudioed.police.man.data.MessageContent;
-import tech.claudioed.police.man.grpc.MessageData;
-import tech.claudioed.police.man.grpc.RegistryID;
-import tech.claudioed.police.man.grpc.VertxPoliceOfficerGrpc;
 import tech.claudioed.police.man.service.HealthService;
 import tech.claudioed.police.man.service.RegistryService;
-
-import java.util.UUID;
 
 public class StartServer extends AbstractVerticle {
 
